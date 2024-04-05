626b234a12fb0682c409094d93467b5991067f9d
mayankchugh-learning <mayankchugh.learning@gmail.com>
2024-04-05 18:42:55 +0800

    data transformation & flowchart

--- ner/constants/__init__.py
+++ ner/constants/__init__.py
@@ -16,3 +16,14 @@ CSV_DATA_FILE_NAME = "ner.csv"
 GCP_MODEL_NAME = "model.pt"
 
 DATA_INGESTION_ARTIFACTS_DIR = "DataIngestionArtifacts"
+
+
+# data transformations
+
+DATA_TRANSFORMATION_ARTIFACTS_DIR = "DataTransformationArtifacts"
+LABELS_TO_IDS_FILE_NAME = "labels_to_ids.pkl"
+IDS_TO_LABELS_FILE_NAME = "ids_to_labels.pkl"
+DF_TRAIN_FILE_NAME = "df_train.pkl"
+DF_VAL_FILE_NAME = "df_val.pkl"
+DF_TEST_FILE_NAME = "df_test.pkl"
+UNIQUE_LABELS_FILE_NAME = "unique_labels.pkl"
--- ner/entity/config_entity.py
+++ ner/entity/config_entity.py
@@ -17,3 +17,32 @@ class DataIngestionConfig:
             self.data_ingestion_artifacts_dir, CSV_DATA_FILE_NAME
         )
 
+
+@dataclass
+class DataTransformationConfig:
+    def __init__(self):
+        self.data_transformation_artifacts_dir: str = os.path.join(
+            ARTIFACTS_DIR, DATA_TRANSFORMATION_ARTIFACTS_DIR
+        )
+        self.labels_to_ids_path: str = os.path.join(
+            self.data_transformation_artifacts_dir, LABELS_TO_IDS_FILE_NAME
+        )
+        self.ids_to_labels_path: str = os.path.join(
+            self.data_transformation_artifacts_dir, IDS_TO_LABELS_FILE_NAME
+        )
+        self.ids_to_labels_gcp_path: str = os.path.join(
+            self.data_transformation_artifacts_dir
+        )
+        self.df_train_path: str = os.path.join(
+            self.data_transformation_artifacts_dir, DF_TRAIN_FILE_NAME
+        )
+        self.df_val_path: str = os.path.join(
+            self.data_transformation_artifacts_dir, DF_VAL_FILE_NAME
+        )
+        self.df_test_path: str = os.path.join(
+            self.data_transformation_artifacts_dir, DF_TEST_FILE_NAME
+        )
+        self.unique_labels_path: str = os.path.join(
+            self.data_transformation_artifacts_dir, UNIQUE_LABELS_FILE_NAME
+        )
+
